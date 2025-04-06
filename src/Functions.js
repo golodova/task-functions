2e474da4761b6fa9d808ae9c02cba8009d4f7936
golodova <golodn03@mail.ru>
2025-04-06 17:05:53 +0300

    Задание по функциям

--- src/Functions.js
+++ src/Functions.js
@@ -22,8 +22,8 @@ export function hasSubstring(substr) {
 console.log(multiply(5)(10));
 // 50
  */
-export function multiply(num) {
+export function multiply(num1) {
     return function(num2) {
-    return num1 * num2;
-  };
-}
+      return num1 * num2;
+    };
+  }
